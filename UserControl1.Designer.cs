be4517a949a470c1c4762142f578b7dd0db7526f
m w <19iceman79@gmail.com>
2020-02-24 20:46:11 +0100

    init

--- UserControl1.Designer.cs
+++ UserControl1.Designer.cs
@@ -28,11 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(322, 106);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(146, 64);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.MouseDown += new System.Windows.Forms.MouseEventHandler(this.volumeGrabBT_MouseDown);
+            this.button11.MouseMove += new System.Windows.Forms.MouseEventHandler(this.volumeGrabBT_MouseMove);
+            this.button11.MouseUp += new System.Windows.Forms.MouseEventHandler(this.volumeGrabBT_MouseUp);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(322, 176);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(146, 64);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(322, 246);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(146, 64);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(322, 316);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(146, 64);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 64);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 73);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(146, 64);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(157, 73);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 64);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(170, 316);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(146, 64);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(157, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 64);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(572, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 64);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(474, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.82716F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56.17284F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(309, 234);
+            this.tableLayoutPanel1.TabIndex = 12;
+            this.tableLayoutPanel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TableLayoutPanel1_MouseMove);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.Load += new System.EventHandler(this.UserControl1_Load_1);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- UserControl1.cs
+++ UserControl1.cs
@@ -16,5 +16,85 @@ namespace WindowsFormsControlLibrary1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+             Button b = sender as Button;             
+             Point p = new Point(b.Top, b.Left);
+             MessageBox.Show(b.Text + "pozycja -x:" + b.Top + " y:" + b.Left);
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Enter(object sender, EventArgs e)
+        {
+            Button Przycisk = sender as Button;
+            MessageBox.Show("jestem" + Przycisk.Text);
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+          //  Button b = sender as Button;
+          //  Point p = new Point(b.Top, b.Left);
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        {
+           Button b = sender as Button;
+            Point nowy_point = PointToClient(Cursor.Position);
+          //  Point p = new Point(b.Top, b.Left);
+            b.Left = nowy_point.X;
+            b.Top = nowy_point.Y;
+
+        }
+
+        private void UserControl1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TableLayoutPanel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+          
+        }
+
+        bool mouseDown = false;
+
+        private void volumeGrabBT_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDown = true;
+            }
+        }
+
+        private void volumeGrabBT_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDown = false;
+            }
+        }
+
+        private void volumeGrabBT_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown == true)
+            {
+                Button b = sender as Button;
+                Point volumeBTPoint = new Point();
+                volumeBTPoint.X = Cursor.Position.X;
+                volumeBTPoint.Y = b.Location.Y;
+                b.Location = volumeBTPoint;
+            }
+        }
     }
 }
